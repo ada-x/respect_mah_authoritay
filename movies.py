dff2de5047186351fadc2b43ea36b8bda8c6ac28
xoxi ixchel <xoxi.ixchel@gmail.com>
2016-06-15 12:17:41 -0700

    Update movies.py

--- movies.py
+++ movies.py
@@ -1,8 +1,3 @@
-# 3 steps as i see them:
-# (1) make a file for the fres_tomatoes.py OR download it into sep file
-# (2) file where movies class is defined, with doc
-# (3) file where objects are defined and functions called, with doc
-
 import webbrowser
 
 
