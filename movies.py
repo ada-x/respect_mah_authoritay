9c6dc08495b00a8d197197297064a7ffecb5b162
xoxi ixchel <xoxi.ixchel@gmail.com>
2016-06-15 00:59:00 -0700

    Add files via upload

--- movies.py
+++ movies.py
@@ -8,9 +8,9 @@ import webbrowser
 
 class Movie:
 
-    def __init__(self, title, trailer_youtube_url, storyline, poster_image_url):
+    def __init__(self, title, trailer_url, storyline, poster_image_url):
         self.title = title
-        self.trailer_youtube_url = trailer_youtube_url
+        self.trailer_youtube_url = trailer_url
         self.storyline = storyline
         self.poster_image_url = poster_image_url
 
